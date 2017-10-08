c67c1868472acf158cd7e75261105848d750dd59
Josh Gao <josh@insolit.us>
2017-10-08 03:08:33 -0700

    Move meson.build to root.

--- cross/setup.sh
+++ cross/setup.sh
@@ -6,7 +6,7 @@ rm -r build.32 build.64
 mkdir build.32 build.64
 
 cd build.32
-meson .. --cross-file ../cross-x86.txt
+meson ../.. --cross-file ../cross-x86.txt
 
 cd ../build.64
-meson .. --cross-file ../cross-x86_64.txt
+meson ../.. --cross-file ../cross-x86_64.txt
