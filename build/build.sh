a046d1eca0a26e9cf6f3c4aea3ab31ad458c1b9f
Josh Gao <josh@insolit.us>
2018-10-27 21:02:46 -0700

    Add a clang toolchain definition file, and switch to it.

--- build/build.sh
+++ build/build.sh
@@ -6,6 +6,6 @@ unset CC
 unset CXX
 
 cd $(dirname $(realpath $0))
-./generate.sh
+./generate.sh $@
 ninja -C i686 install
 ninja -C x86_64 install
--- build/generate.sh
+++ build/generate.sh
@@ -5,6 +5,12 @@ set -ex
 unset CC
 unset CXX
 
+if [ "$1" == "--use-gcc" ]; then
+  TOOLCHAIN_PREFIX="toolchain-gcc"
+else
+  TOOLCHAIN_PREFIX="toolchain-clang"
+fi
+
 cd $(dirname $(realpath $0))
 
 [ -e i686 ] && rm -r i686
@@ -12,5 +18,5 @@ cd $(dirname $(realpath $0))
 
 mkdir i686 x86_64
 
-(cd i686; meson ../.. --cross-file ../../toolchain/toolchain-gcc-i686.txt)
-(cd x86_64; meson ../.. --cross-file ../../toolchain/toolchain-gcc-x86_64.txt)
+(cd i686; meson ../.. --cross-file ../../toolchain/${TOOLCHAIN_PREFIX}-i686.txt)
+(cd x86_64; meson ../.. --cross-file ../../toolchain/${TOOLCHAIN_PREFIX}-x86_64.txt)
