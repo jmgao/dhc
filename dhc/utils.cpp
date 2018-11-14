d7746a470c7616b8d7735539335fd5336100dece
Josh Gao <josh@insolit.us>
2018-11-14 00:56:15 -0800

    Convert unknown GUIDs to their values in to_string.

--- dhc/utils.cpp
+++ dhc/utils.cpp
@@ -104,7 +104,12 @@ std::string to_string(REFGUID guid) {
   } else if (guid == GUID_Unknown) {
     return "GUID_Unknown";
   } else {
-    return "<unknown>";
+    char guid_string[37];  // 32 hex chars + 4 hyphens + null terminator
+    snprintf(guid_string, sizeof(guid_string) / sizeof(guid_string[0]),
+             "{%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", guid.Data1, guid.Data2,
+             guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4],
+             guid.Data4[5], guid.Data4[6], guid.Data4[7]);
+    return guid_string;
   }
 }
 
