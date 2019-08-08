249d8f49fad148625d49d46dfa4f152a509af0f1
Josh Gao <josh@jmgao.dev>
2019-08-07 21:55:08 -0700

    Fix printing of unknown GUIDs.

--- dinput8/utils.cpp
+++ dinput8/utils.cpp
@@ -151,7 +151,7 @@ std::string to_string(REFGUID guid) {
   } else if (guid == GUID_Unknown) {
     return "GUID_Unknown";
   } else {
-    char guid_string[37];  // 32 hex chars + 4 hyphens + null terminator
+    char guid_string[64];  // 32 hex chars + 4 hyphens + 2 braces + null terminator
     snprintf(guid_string, sizeof(guid_string) / sizeof(guid_string[0]),
              "{%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}", guid.Data1, guid.Data2,
              guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4],
